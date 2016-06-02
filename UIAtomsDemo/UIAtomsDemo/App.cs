afa5cf531511060238baad1ea106a8762926c376
neurospeech <support@neurospeech.com>
2016-06-02 23:28:21 +0530

    Work in progress - setup pending

---
+++ UIAtomsDemo/UIAtomsDemo/App.cs
@@ -0,0 +1,34 @@
+﻿using Form;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace UIAtomsDemo
+{
+	public class App : Application
+	{
+		public App ()
+		{
+            // The root page of your application
+            MainPage = new SimpleForm();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
